c2fdc4a943331613481484c74c1fb93cd7ff225c
Jakub Zajkowski <jakudjan123@gmail.com>
2023-10-11 17:48:47 +0200

    feat: implement game logic

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -8,17 +8,55 @@ import {Root} from "../../services/models/currency.model";
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit{
-
+  public currency! : Root;
+  public inputValue : string="";
+  public message! : string;
+  public correct! : boolean;
+  public result: number;
   constructor(private currencyService: CurrencyService) {
-
+    this.result=0
   }
 
-  ngOnInit() {
-      this.currencyService.fetchData().subscribe(value => this.currencyService.setCurrency(value))
+  ngOnInit(): void {
+    this.fetchData()
   }
-
-  getCurrency() : number{
-    return this.currencyService.getCurrency()
+  setInputValue(name: string):void {
+    this.inputValue=  name;
+  }
+  check():void{
+    const currencyValue = this.currency.rates[0].bid
+    if (currencyValue==parseFloat(this.inputValue)){
+      this.message="Good Job You Guess!";
+      this.correct=true;
+      this.result+=1
+    }
+    else if (currencyValue<parseFloat(this.inputValue)){
+      this.message="Your Value is To High!";
+      this.correct=false;
+      this.result+=1
+    }
+    else if (currencyValue>parseFloat(this.inputValue)){
+      this.message="Your Value is To Low!";
+      this.correct=false;
+      this.result+=1
+    }
+    this.inputValue=""
+  }
+  getGuessClass() {
+    return {
+      'correct-guess': this.correct,
+      'incorrect-guess': !this.correct
+    };
+  }
+  fetchData(){
+    this.currencyService.fetchData().subscribe(value => {
+      const data = value
+      data.rates[0].bid = Math.round(data.rates[0].bid*100)/100
+      this.currency = data
+    })
+    this.result=0
+    this.correct=false
+    this.message=""
   }
 
 }
--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -10,17 +10,11 @@ export class CurrencyService {
   private currency : Root = {} as Root
   private currencies: string[];
   constructor(private httpClient: HttpClient) {
-    this.currencies = ['usd','chf','jpy','aud','cad','cny','eur','gbp']
+    this.currencies = ['usd','chf','jpy','aud','cad','eur','gbp']
   }
 
   fetchData():Observable<Root>{
     const randomCurrency = this.currencies[Math.floor(Math.random()*7)]
     return this.httpClient.get<Root>(`https://api.nbp.pl/api/exchangerates/rates/c/${randomCurrency}/2023-10-11/?format=json`)
-  }
-  setCurrency(data:Root):void{
-    this.currency=data
-  }
-  getCurrency(): number{
-    return this.currency.rates[0].bid
   }
 }
